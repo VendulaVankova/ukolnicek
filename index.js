413d125aadbd7bf916a5b0d54c4b9b0edfaaf69e
VendulaVankova <vendula.van@gmail.com>
2022-10-29 22:43:55 +0200

    Pár slov k úkolu

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log('funguju!');
+
+/* Mrzí mne, že odevzdávám nezpracovaný úkol. Bohužel jsem neodhadla své síly se čtyřměsíčním miminkem a jsem tak v probírané látce významně pozadu. Postupně se snažím vše dohánět, i díky perfektním materiálům, které nám byly zpřístupněny, mé tempo je ale závratně pomalé. Díky za čas věnovaný přečtení této zprávy a omlouvám se!
